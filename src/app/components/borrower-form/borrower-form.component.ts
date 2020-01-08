5c99fe8738f94bcf3e47d22017890e1fe9b65711
rocnwall <christianbeard.bnp@gmail.com>
2020-01-08 15:56:50 +0100

    Gestion du multi-emprunteurs

--- src/app/components/borrower-form/borrower-form.component.ts
+++ src/app/components/borrower-form/borrower-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MortgageProjectService } from 'src/app/services/mortgage-project.service';
 import { Borrower } from 'src/app/Models/borrower';
@@ -13,41 +13,69 @@ import { Project } from 'src/app/Models/mortgage-project';
 export class BorrowerFormComponent implements OnInit {
 
   borrowerForm: FormGroup;
+  public borrowerList: FormArray;
+
   @Input() project: Project;
   borrower: Borrower;
 
   @Output() submitNext = new EventEmitter<Project>();
 
   constructor(private fb: FormBuilder,
-              private router: Router,
-              private projectService: MortgageProjectService) { }
+    private router: Router,
+    private projectService: MortgageProjectService) { }
 
   ngOnInit() {
     this.borrowerForm = this.fb.group({
+      borrowers: this.fb.array([this.newBorrower()])
+    });
+    this.borrowerList = this.borrowerForm.get('borrowers') as FormArray;
+  }
+
+  getBorrowers(): FormArray {
+    return this.borrowerForm.get('borrowers') as FormArray;
+  }
+
+  newBorrower(): FormGroup {
+    return this.fb.group({
       dateOfBirth: ['1980-01-01', Validators.required],
       netIncome: ['10']
     });
-    console.log('borrowerForm');
-    console.log(this.project);
+  }
+
+  addBorrower() {
+    this.borrowerList.push(this.newBorrower());
+  }
+
+  removeBorrower(i: number) {
+    this.borrowerList.removeAt(i);
+  }
+
+  onSubmit() {
+    console.log(this.borrowerForm.value);
   }
 
   saveBorrower() {
-    // Transforme les données du formulaire en instance de Projet
     const formData = this.borrowerForm.value;
-    this.borrower= new Borrower({
-      dateOfBirth: formData.dateOfBirth,
-      netIncome: formData.netIncome
-    });
-    console.log(this.borrower);
-    this.project.borrowers.push(this.borrower);
+    for (let borrower of formData.borrowers) {
+      // for (let i = 0; i < this.borrowerList.length; i++) {  //How to properly iterate here!!
+      console.log(borrower);
+      console.log('toto');
+    }
+    //   // Transforme les données du formulaire en instance de Projet
+    //   const formData = this.borrowerForm.value;
+    //   this.borrower = new Borrower({
+    //     dateOfBirth: formData.dateOfBirth,
+    //     netIncome: formData.netIncome
+    //   });
+    //   console.log(this.borrower);
+    this.project.borrowers = formData.borrowers;
 
     // Sauvegarde l'instance du projet.
 
     console.log(this.project);
 
-    this.projectService.updateMortgageProject(this.project).subscribe( () => {
-      // Confirmation
-      // alert('Projet bien enregistré !');
+    this.projectService.updateMortgageProject(this.project).subscribe(data => {
+      this.project.maxLoanPayment = data.maxLoanPayment;
       this.submitNext.emit(this.project);
     });
   }
